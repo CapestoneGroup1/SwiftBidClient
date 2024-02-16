7cd9c5ac947168559c2d671fbe744fd382b58ad2
pullapavankumar <pullapavankumar.ca@gmail.com>
2024-02-16 10:11:40 -0500

    Footer changes

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import AppRoutes from "../pages/routes/AppRoutes";
 import Footer from "./Footer";
+import styles from '../assets/css/AppLayout.module.css';
 
 export default function AppLayout() {
   return (
     <>
       <Header />
-      <AppRoutes />
+      <main className={styles.main}>
+        <AppRoutes />
+      </main>
       <Footer />
     </>
   );
